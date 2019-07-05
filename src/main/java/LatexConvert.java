4bb8f42e9e8ef3f9e3853f8470da7727d18c2980
yyWang <1473332533@qq.com>
2019-07-05 17:40:06 +0800

    创建LaTeXConvert主类

---
+++ src/main/java/LatexConvert.java
@@ -0,0 +1,14 @@
+/**
+ * @author ：yyWang
+ * @date ：Created in 2019/7/5
+ * @description： 将文本中 英文字母用LaTeX 的 $ 进行包裹
+ */
+public class LatexConvert {
+
+
+    public static void main(String[] args) {
+        String str = "";
+    }
+
+
+}
