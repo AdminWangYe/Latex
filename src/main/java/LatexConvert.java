d2c64687b1f5b577a7e7759cd3afb468ec00f336
Chaconez <32936025+Chaconez@users.noreply.github.com>
2019-07-12 21:05:24 +0800

    修复一些逻辑错误，并解决实体LaTeX化被后面元素覆盖的问题

--- src/main/java/LatexConvert.java
+++ src/main/java/LatexConvert.java
@@ -8,23 +8,23 @@ import java.util.regex.Pattern;
  * @description： 将文本中 英文字母用LaTeX 的 $ 进行包裹
  * 思路：
  * 1. 先用正则进行中文匹配，然后使用中文进行分割
+ * 2. 处理一些无用的字符，一些标点符号。
+ * 3. 处理需要LaTeX化的实体，并将替换的结果保存到list 集合中。
+ * 4. 对应文本根据list保存的内容进行替换，但是每个实体LaTeX化后都需要用其他字符代替。以免后面的结果覆盖当前内容
+ * 5. 将用其他字符替换后文本恢复LaTeX化实体，处理完成，返回结果
  */
 public class LatexConvert {
 
-    // 保存替换后的字符串
-    private Map<String, String> keymap = new LinkedHashMap<>();
+    // 保存替换后的字符串,可以保存重复的键值对
+    private List<Map<String, String>> keymap = new ArrayList<>();
     // 将转LaTeX化的实体进行替换成 replaceLatex 集合中的字符
     private Map<String, String> replaceMap = new LinkedHashMap<>();
     // 从替换的元素中任选一个进行替换
     private static Random random = new Random();
-    // 匹配 LaTeX 公式，
-    public static final String LatexExpress = "\\$.*?\\$";
-    // 匹配 ；类似∠1+∠2+∠3+∠4=100或者100°的表达式
-    public static final String AngleExpress = "∠[0-9]+?\\+?∠[0-9]+?\\+?∠[0-9]+?\\+?∠[0-9]+?=\\d+°?";
 
+    //    匹配中文字符
     private static final String ChinanesStr = "[\\u4e00-\\u9fa5_]+:?";
 
-    public static final String SegmentExpress = "";
 
     // LaTeX 用下面的词进行替换，最后再转换
     private static final List<String> replaceLatex = new ArrayList<String>(Arrays.asList(
@@ -33,6 +33,7 @@ public class LatexConvert {
             "溪边", "文鳐", "白雉", "梼杌", "天之痕", "轩辕剑", "神农鼎", "炼妖壶", "昆仑镜", "崆峒", "乾坤鼎", "西王母", "洪荒", "盘古幡", "诛仙剑阵"
     ));
 
+    //    需要删除的符号
     private List<String> deleStr = new ArrayList<>(Arrays.asList(
             "", "(", ",", "“", "”", ")", "()"
     ));
@@ -48,12 +49,15 @@ public class LatexConvert {
         // 每次处理一个新的文本都需要将 map 集合里的数据清空
         keymap.clear();
         replaceMap.clear();
+        //  步骤1. 按照中文进行切分
         Pattern pattern = Pattern.compile(ChinanesStr);
         String[] strs = pattern.split(str.trim());
         List<String> strset = new ArrayList<>(Arrays.asList(strs));
-        // 删除一些标点符号，统一格式
+
+        // 步骤2.删除一些标点符号，统一格式
         strset.removeAll(deleStr);
-        // 遍历集合并进行替换
+
+        // 步骤3.处理按照中文切分后的字符串，并按照格式再次切分，切割成只有单个字符或者实体
         for (String string : strset) {
             dealStr(string);
         }
@@ -61,20 +65,25 @@ public class LatexConvert {
         // 将已经使用的字符移除掉
         List<String> stringList = replaceLatex;
 
-        // 按照文本中的实体，替换首次匹配到的实体
-        for (Map.Entry<String, String> entry : keymap.entrySet()) {
-            str = str.replaceFirst(Pattern.quote(entry.getKey()), Matcher.quoteReplacement(entry.getValue()));
-            // 虽然按照首次匹配的正则进行替换，但是还会出现后面覆盖前面实体LaTeX化的内容，这时可以将已经 LaTeX化的实体利用其它字符进行替换。
-            str = replaceStr(str, stringList);
+        // 步骤4.按照文本中的实体，替换首次匹配到的实体
+        for (Map<String, String> entity : keymap) {
+            for (Map.Entry<String, String> edit : entity.entrySet()) {
+                str = str.replaceFirst(Pattern.quote(edit.getKey()), Matcher.quoteReplacement(edit.getValue()));
+                // 虽然按照首次匹配的正则进行替换，但是还会出现后面覆盖前面实体LaTeX化的内容，这时可以将已经 LaTeX化的实体利用其它字符进行替换。
+                str = replaceStr(str, stringList);
+            }
+
         }
 
+
+        // 替换成原字符串
         str = replaceEndStr(str);
 
         return str;
     }
 
     /**
-     * 将用其他字符替换的 LaTeX化的实体，进行还原，成原字符串
+     * 步骤5 将用其他字符替换的 LaTeX化的实体，进行还原，成原字符串
      *
      * @param str 需要替换的字符串
      * @return 返回替换后的字符串
@@ -87,7 +96,7 @@ public class LatexConvert {
     }
 
     /**
-     * 将已经转成 LaTeX 的实体进行其他字符替代以免被覆盖
+     * 步骤4.将已经转成 LaTeX 的实体进行其他字符替代以免被覆盖
      *
      * @param str        需要处理的字符串
      * @param stringList 保留关键字的集合
@@ -123,26 +132,30 @@ public class LatexConvert {
     }
 
     /**
+     * 步骤3
      * 对list集合里的字符串进行处理，并分割成单个字符。进行实体 LaTeX 化
-     * 处理后的结果保存在keymap 全局集合中返回处理的 map 集合，就是处理过的字符串，关键字是原字符串，对应的值是替换后的 LaTeX实体
+     * 处理后的结果保存在keymap 全局集合中返回处理的 list 集合，就是处理过的字符串，关键字是原字符串，对应的值是替换后的
+     * LaTeX实体，再将这个map 放进keymap list 集合中，因为list集合允许元素重复。当一个题目中有多个重复的实体，后面进行
+     * 处理时就会被替换。文本中有多少个需要转化的实体，list就有多少个元素，但是用hashmap 进行存储时元素个数就会小于这个值。
      *
      * @param string 需要处理的字符串
      */
     private void dealStr(String string) {
         // 如果是坐标的话就按照坐标进行切分，否则就按照逗号或者顿号进行切割
-        String[] old = string.split("[A-z]+\\([A-z0-9],[A-z0-9]\\)|、|,");
+        String[] old = string.split("[A-z]+\\([A-z0-9],[A-z0-9]\\)|、|,|，");
         if (old.length > 1) {
             for (String str : old) {
                 // 去掉字符串的前后空格的影响
                 str = str.trim();
-                // 如果包含字符串包含$，说明该字符串是LaTeX格式，不需要进行处理
-//                String newStr = newStrReplace(str);
-//                keymap.put(str, newStr);
+                // 递归调用进行 实体LaTeX化
                 dealStr(str);
             }
-        } else {
-            String replaceStr = newStrReplace(string);
-            keymap.put(string, replaceStr);
+        } else if (!old[0].equals("")) {
+            String replaceStr = newStrReplace(old[0]);
+            Map<String, String> keyValue = new HashMap<>();
+            keyValue.put(old[0], replaceStr);
+            // 将原实体添加到 map集合中，并保存对应的 键值对，key是原字符串，value是 LaTeX化的实体。然后将这个map 添加到list 集合中。
+            keymap.add(keyValue);
         }
 
     }
@@ -157,12 +170,8 @@ public class LatexConvert {
         if (!str.contains("$") && !str.equals("")) {
             if (str.contains("=")) {
                 str = "$" + str + "$";
-            } else if (str.contains("△") || str.contains("∥")) {
-                str = str.replaceAll("[△∥]?([A-Z']+)", "\\$$1\\$");
-            } else if (str.contains("∠")) {
-                str = str.replaceAll("∠([A-z]+)", "\\$$1\\$");
-            } else if (str.contains("⊥")) {
-                str = str.replaceAll("([A-Z]+)", "\\$$1\\$");
+            } else if (str.contains("△") || str.contains("∥") || str.contains("⊥") || str.contains("∠")) {
+                str = str.replaceAll("([A-z']+)", "\\$$1\\$");
             } else if (!patternNum(str)) {
                 str = "$" + str + "$";
             }
@@ -173,7 +182,7 @@ public class LatexConvert {
     public static void main(String[] args) {
 
         LatexConvert latexConvert = new LatexConvert();
-        String str = "四边形ABCD的对角线AC,BD交于点O,已知O是AC的中点,AE=CF,DF∥BE, 证明:△BOE≌△DOF, 若OD=1/2AC,则四边形ABCD是什么特殊四边形,请证明你的结论";
+        String str = "△MNP中,∠P=60°,MN=NP,MQ⊥PN,垂足为Q,延长MN至G,使NG=NQ,若△MNP的周长为12,MQ=a,则△MGQ的周长是";
         System.out.println("原字符串：" + str);
         System.out.println(latexConvert.getPattern(str));
 
